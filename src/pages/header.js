c55367b3988aa789dd243fbec0877098aad4d7a0
JimRnst <jrnst09@gmail.com>
2022-10-12 21:55:03 -0500

    first commit

---
+++ src/pages/header.js
@@ -0,0 +1,22 @@
+const createHeader = function(){
+    const create = document.createElement("header");
+    create.innerHTML = `
+            <div id="call">Call us (555) 555 5555</div>
+            <div id="menu">
+                <div id="menu-left">
+                    <a href="#" id="home">Home</a>
+                    <a href="#" id="pmenu">Menu</a>
+                    <a href="#" id="about">About</a>
+                </div>
+                <div id="logo"><img src="/assets/gudcake.png"></div>
+                <div id="menu-right">
+                    <a href="#" id="contact">Contact Us</a>
+                    <div id="cart"><i class="fa-solid fa-cart-shopping"></i></div>
+                </div>
+            </div>
+    `
+document.getElementById("container").appendChild(create);
+};
+
+
+export default createHeader;
---
+++ src/pages/home.js
@@ -0,0 +1,48 @@
+//CREAR PAGINA PRINCIPAL
+const home = function(){
+    const createHomePage = document.createElement("main");
+    createHomePage.innerHTML = `
+            <div id="carrousel">
+                <div id="btn-img">
+                    <i class="fa-solid fa-circle-chevron-left" id="left" style="color: white;"></i>
+                    <i class="fa-solid fa-circle-chevron-right" id="right" style="color: white;"></i>
+                </div>
+                <img id= "cake" src="/assets/cake1.jpg" alt="cake">
+            </div>
+            <div class="main-menu">
+                <div class="title" id="main-about">ABOUT US</div>
+                <div class="photo"><img src="/assets/chef.jpg"></div>
+                <div class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam accusamus molestiae tenetur facilis, laudantium id ipsam laborum quod eos similique porro numquam vero quas praesentium amet deleniti neque repellat, placeat hic. Quasi laboriosam in fugit quis sed odio modi neque qui voluptate, dolorem hic sequi adipisci, deserunt vero id facere.</div>
+            </div>
+    `
+document.getElementById("container").appendChild(createHomePage);
+carrousel();
+};
+
+ const carrousel = (function(){
+     const clicked = document.querySelector("#btn-img");
+     const imgChanged = document.querySelector("#cake")
+     const carrouselImages = ["/assets/cake1.jpg", "/assets/cake2.jpg", "/assets/cake3.jpg", "/assets/cake4.jpg", "/assets/cake5.jpg", "/assets/cake6.jpg"]
+     let countImg = 0
+     clicked.addEventListener("click", (e) => {
+         if(e.target.id === "left"){
+             if(countImg > 0){
+                 imgChanged.src = carrouselImages[countImg -1];
+                 countImg--;
+             } else{
+                 imgChanged.src = carrouselImages[carrouselImages.length -1]
+                 countImg = carrouselImages.length -1;
+             }
+         } else if(e.target.id === "right"){
+             if(countImg < carrouselImages.length -1){
+                 imgChanged.src = carrouselImages[countImg +1];
+                 countImg++
+             } else{
+                 imgChanged.src = carrouselImages[0];
+                 countImg = 0
+             }
+         }
+     });
+ });
+ 
+export default home;
